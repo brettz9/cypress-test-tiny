79b2f6777f2a8bb50bb797dccd64bf042f8efc4b
Brett Zamir <brettz9@yahoo.com>
2020-03-09 12:52:29 +0800

    - Also doesn't work to inspect on `validationMessage`

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -20,6 +20,17 @@ describe('page', () => {
     });
   });
 
+  it('should catch invalid by JS API (validationMessage)', function () {
+      cy.visit('http://localhost:8000/');
+      const tooShortOfAName = 'a';
+      cy.get('[data-name="name"]').type(tooShortOfAName);
+      // cy.get('[data-name="action2"]').click();
+
+      cy.get('[data-name="name"]').then(($input) => {
+        return expect($input[0].validationMessage.length).to.not.equal(0);
+      });
+  });
+
   it('should obtain environmental variables', function () {
       cy.log(Cypress.env('secret'))
       expect(Cypress.env('secret')).to.equal('Be good');
