78653e1f8194fd3562ace1e8f0409add40940b78
Brett Zamir <brettz9@yahoo.com>
2020-03-09 12:59:55 +0800

    - Should catch invalid elements by `checkValidity`

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -31,6 +31,17 @@ describe('page', () => {
       });
   });
 
+  it('should catch invalid by JS API (checkValidity())', function () {
+      cy.visit('http://localhost:8000/');
+      const tooShortOfAName = 'a';
+      cy.get('[data-name="name"]').type(tooShortOfAName);
+      // cy.get('[data-name="action2"]').click();
+
+      cy.get('[data-name="name"]').then(($input) => {
+        return expect($input[0].checkValidity()).to.be.false;
+      });
+  });
+
   it('should obtain environmental variables', function () {
       cy.log(Cypress.env('secret'))
       expect(Cypress.env('secret')).to.equal('Be good');
